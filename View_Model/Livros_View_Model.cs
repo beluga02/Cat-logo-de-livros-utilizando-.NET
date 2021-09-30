bfdb300154ca167becbe41d2d6e22d7394e2c151
Letícia Matias <66281122+beluga02@users.noreply.github.com>
2021-09-30 17:37:23 -0300

    Create Livros_View_Model.cs

---
+++ View_Model/Livros_View_Model.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Projeto22.View_Model
+{
+    public class Livros_View_Model
+    {
+        public Guid Id { get; set; }
+        public string nome_livro { get; set; }
+        public string autor { get; set; }
+        public double preco { get; set; }
+    }
+}
